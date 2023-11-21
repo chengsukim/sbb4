d2a0e4f08180050b89f572b7a014355f7b1d6705
깃chengsukim <kcs981123@naver.com>
2023-11-21 12:19:29 +0900

    sbb4

---
+++ src/test/java/com/sdd/sbb4/Sbb4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sdd.sbb4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sbb4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
